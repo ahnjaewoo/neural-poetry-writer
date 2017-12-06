fd1c00f74cac5a794c64cab71d4f01148b74a438
sdw0316 <sdw0316@gmail.com>
2017-12-06 11:46:15 +0900

    RNN training

---
+++ rnn/predict.py
@@ -0,0 +1,10 @@
+# generate a text with given input
+
+import torch
+import os
+
+from model import *
+
+def predict(model, start_str="A", predict_len=100, temperature=0.8):
+
+def generate(model_path, start_str):
--- rnn/train.py
+++ rnn/train.py
@@ -1,29 +1,79 @@
 # Training phase with RNN model
-
+import random
 import torch
 import torch.nn as nn
+import time
+import os
 from torch.autograd import Variable
 
 from model import *
+from predict import *
 
-def random_training_set(sequence_len, batch_size):
-	#
+# get a sequence in a data file randomly
+def random_sequence (sequence_len):
+	start_index = random.randint(0, data_len - sequence_len)
+	end_index = start_index + sequence_len
+	return data[start_index:end_index]
 
-	input = torch.LongTensor(batch_size, sequence_len)
-    output = torch.LongTensor(batch_size, sequence_len)
+# make a mini batch
+def random_training_set (sequence_len, batch_size):
+	X = torch.LongTensor(batch_size, sequence_len)
+    Y = torch.LongTensor(batch_size, sequence_len)
     for batch_iter in range(batch_size):
-
-    for bi in range(batch_size):
-        start_index = random.randint(0, file_len - chunk_len)
-        end_index = start_index + chunk_len + 1
-        chunk = file[start_index:end_index]
+    	sequence = random_sequence(sequence_len)
         # takes all characters in one sequence as an input
-        input_data[bi] = char_tensor(chunk[:-1])
-        # all characters (except the first one) in one sequecne are expected output
-        target[bi] = char_tensor(chunk[1:]) 
-    inp = Variable(inp)
-    target = Variable(target)
-    if args.cuda:
-        inp = inp.cuda()
-        target = target.cuda()
-    return inp, target
+        X[batch_iter] = sequence 
+        # all characters (except the first one) in one sequence are expected output
+        X[batch_iter] = # TODO 
+
+    X = Variable(X)
+    Y = Variable(Y)
+    
+    return X, Y
+
+# save trained model
+def save(model, file_name):
+    save_filename = os.path.splitext(os.path.basename(file_name))[0] + '.pt'
+    torch.save(model, save_filename)
+    print('Saved as %s' % save_filename)
+
+# Training Phase 
+def train_run (input_size, hidden_size, output_size, sequence_len, batch_size=1, n_layers=1, n_epochs=-1, cycle=100)
+	decoder = RNN(input_size, hidden_size, output_size, n_layers=1)
+	start = time.time()
+	all_losses = []
+	loss_avg = 0
+
+	# Data loader -> Train
+
+	try:
+		if (n_epochs = -1):
+			epoch = 0
+			while (true):
+				epoch++
+				loss = train(*random_training_set(sequence_len, batch_size))
+      	loss_avg += loss
+
+      	if epoch % cycle == 0:
+          print('[Time : %s, Epoach : (%d), Loss : %.4f]' % (time_since(start), epoch, loss))
+          save()
+          print(generate(decoder, 'Wh', 100, cuda=args.cuda), '\n')
+
+		else :
+    	print("Training for %d epochs..." % n_epochs)
+    	for epoch in tqdm(range(1, args.n_epochs + 1)):
+	    	loss = train(*random_training_set(args.chunk_len, args.batch_size))
+	    	loss_avg += loss
+
+	    	if epoch % args.print_every == 0:
+	        	print('[%s (%d %d%%) %.4f]' % (time_since(start), epoch, epoch / args.n_epochs * 100, loss))
+	        	print(generate(decoder, 'Wh', 100, cuda=args.cuda), '\n')
+
+    	print("Saving...")
+    	save()
+
+	except KeyboardInterrupt:
+    	print("Saving before quit...")
+    	save()
+
+
