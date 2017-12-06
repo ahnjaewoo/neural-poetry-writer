69d6b0d1cd48787d8166f9917b770ad7d19bb43e
MOVABLEWORK\ouiqdmw <ouiqdmw17@gmail.com>
2017-12-06 21:08:28 +0900

    data_loader + code_book changes now directory parameter required
    code_book changes
    inverted function for character-number mapping

--- utils/code_book.py
+++ utils/code_book.py
@@ -4,9 +4,12 @@ import numpy as np
 # represent character as a number, vector
 # 
 class code_book():
-	def __init__(self):
+	def __init__(self, midfile_dir):
 		self.vector_size = 0
 		self.codes = dict()
+		self.invert_codes = list()
+
+		self.midfile_dir = midfile_dir
 	# gather - if given character is unknown, create mapping and add it to codes
 	# gather fucntion changes : now count occurence
 	def gather(self, character):
@@ -16,17 +19,20 @@ class code_book():
 			self.codes[character] += 1
 	def sort_codes(self):
 		codes = self.codes
+		invert_codes = self.invert_codes
 		codes_list = sorted(codes, key=lambda k : codes[k], reverse=True)
 		codes_temp = dict()
 		idx = 0
 		for ch in codes_list:
 			codes_temp[ch] = idx
+			invert_codes.append(ch)
 			idx += 1
 		self.codes = codes_temp
+		self.invert_codes = invert_codes
 		self.vector_size = idx
 	# save to a file with specific format, use codes
 	def save_to(self, filename):
-		f = codecs.open(filename, "w", 'utf-8')
+		f = codecs.open(self.midfile_dir +'/'+ filename, "w", 'utf-8')
 		codes = self.codes
 		for key, value in codes.items():
 			f.write(key)
@@ -34,17 +40,19 @@ class code_book():
 	# load from a file of format assumption, form codes
 	def load_from(self, filename):
 		self.codes.clear()
-		f = codecs.open(filename, "r", 'utf-8')
+		self.invert_codes = []
+		f = codecs.open(self.midfile_dir +'/'+ filename, "r", 'utf-8')
 		text = f.read()
 		idx = 0
 		for ch in text:
 			self.codes[ch] = idx
+			self.invert_codes.append(ch)
 			idx += 1
 		self.vector_size = idx
 		f.close()
-        
-	def get_number(self, seq):
-		number = [self.codes[ch] for ch in seq]
+
+	def get_number(self, text):
+		number = [self.codes[ch] for ch in text]
 		return number
 	def get_number_batch(self, batch):
 		number_batch = list()
@@ -57,6 +65,23 @@ class code_book():
 			number_batchs.append(self.get_number_batch(batch))
 		return number_batchs
 	
+	def get_string(self, number_text):
+		text = ""
+		for nch in number_text:
+			text += self.invert_codes[nch]
+		return text
+	def get_string_batch(self, number_batch):
+		batch = list()
+		for number_seq in number_batch:
+			batch.append(self.get_string(number_seq))
+		return batch
+	def get_string_batchs(self, np_batchs):
+		batchs = list()
+		number_batchs = np_batchs.tolist()
+		for number_batch in number_batchs:
+			batchs.append(self.get_string_batch(number_batch))
+		return batchs
+
 	# get numpy vector representation of string, based on codes
 	def get_vectors(self, text):
 		text_num = get_numbers(text)
