364e31b3bcae6c5137b9f328db4405929b6f55ad
sdw0316 <sdw0316@gmail.com>
2017-12-06 21:18:06 +0900

    generation

--- rnn/predict.py
+++ rnn/predict.py
@@ -2,24 +2,33 @@
 import torch
 import os
 
+from torch.autograd import Variable
+
 # prediction
 def predict(model, start_sequence, predict_len=100, temperature=0.8):
-	hidden = model.init_hideen(1)
+	hidden = model.init_hidden(1)
 	sequence_len = len(start_sequence)
-
-	X = torch.LongTensor(sequence_len)
-	X = torch.from_numpy(start_sequence)
+ 
+	X = torch.LongTensor(start_sequence)
 	X = Variable(X)
 
-	predicted = start_sequence
-
+	predicted = []
 	# build up hidden states
 	for i in range(sequence_len-1):
 		_, hidden = model(X[:,i], hidden)
+	# generate new character with last character of given sequence
+	X = X[-1]
 
-	X = X[:, -1]
-
+	# generate a sequence of characters
 	for i in range(predict_len):
 		output, hidden = model(X, hidden)
-	for c in range(len())
-	return predicted
+
+		# sample from the network as a multinomial distribution
+		output_dist = output.data.view(-1).div(temperature).exp()
+		top_vector = torch.multinomial(output_dist, 1)[0]
+
+		# append generated vector to sequence
+		predicted.append(top_vector) 
+
+	# return the generated sequence except input
+	return predicted[sequence_len:]
